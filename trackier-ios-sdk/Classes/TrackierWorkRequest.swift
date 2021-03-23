da1cc6d8e409562f52ec664769e335bbc7394751
prak24oct <prakharatdit@gmail.com>
2021-03-23 18:18:05 +0530

    Update TrackierWorkRequest.swift

--- trackier-ios-sdk/Classes/TrackierWorkRequest.swift
+++ trackier-ios-sdk/Classes/TrackierWorkRequest.swift
@@ -16,6 +16,7 @@ class TrackierWorkRequest {
     var kind: String
     var installId: String = ""
     var installTime : String = ""
+    var installTime: String = ""
     var eventObj = TrackierEvent(id: "")
     var deviceInfo: DeviceInfo? = nil
     var lastSessionTime: String
@@ -35,10 +36,8 @@ class TrackierWorkRequest {
         dict["device"] = deviceInfo?.getDeviceInfo()
         dict["createdAt"] = Utils.getCurrentTime()
         dict["mode"] = self.mode
-        dict["installId"] = self.installId
         dict["installId"] = self.installId.lowercased()
         dict["installTime"] = self.installTime
-        dict["debug"] = true
         return dict
     }
     
