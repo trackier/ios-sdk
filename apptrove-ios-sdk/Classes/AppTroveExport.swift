9436cd1efa900262f61ef8167ecea8cb82c9f65b
Satyam Jha <satyam@trackier.com>
2026-06-04 14:35:18 +0530

    Version Dump to 2.0.5

--- apptrove-ios-sdk/Classes/AppTroveExport.swift
+++ apptrove-ios-sdk/Classes/AppTroveExport.swift
@@ -14,7 +14,7 @@ public func initialize(initParam: UnsafePointer<CChar>?)  {
     let sdkConfigs = AppTroveSDKConfig.init(appToken: initData.appKey, env: initData.env)
     sdkConfigs.sdkt = initData.setSDKType
     sdkConfigs.sdkVersion = initData.setSDKVersion
-    sdkConfigs.setSDKVersion(sdkVersion: "2.0.4")
+    sdkConfigs.setSDKVersion(sdkVersion: "2.0.5")
     AppTroveSDK.initialize(config: sdkConfigs)
 }
 
